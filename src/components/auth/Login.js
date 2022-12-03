6e60ddaca39f0cc682ef42424937271701f8b9ee
Charles Maina Mwangi <38659056+MainaMwangiy@users.noreply.github.com>
2022-12-04 00:11:01 +0300

    add login & register page (#7)

---
+++ src/components/auth/Login.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Paper, Card, Grid, TextField, Button } from "@mui/material";
+
+export const Login = () => {
+  return (
+    <Grid container spacing={2}>
+    <Grid item xs={12}>
+      <TextField
+        id="outlined-basic"
+        label="Email"
+        variant="outlined"
+        fullWidth
+      />
+    </Grid>
+    <Grid item xs={12}>
+      <TextField
+        id="outlined-basic"
+        label="Password"
+        variant="outlined"
+        fullWidth
+      />
+    </Grid>
+    <Button variant="contained" fullWidth >{"Login"}</Button>
+  </Grid>
+  )
+}
