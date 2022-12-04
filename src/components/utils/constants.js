1f5eab3ed882d7f904d2b4a752db1dc7a26af994
Charles Maina Mwangi <38659056+MainaMwangiy@users.noreply.github.com>
2022-12-04 16:48:48 +0300

    Added routing of the files (#8)

---
+++ src/components/utils/constants.js
@@ -0,0 +1,6 @@
+export const navHeaders = [
+    { id: "1", NavValue: "Home", url: "/" },
+    { id: "2", NavValue: "Add New Button", url: "/AddNew" },
+    { id: "3", NavValue: "Login", url: "/Login" },
+  ];
+
