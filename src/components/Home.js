ffd329de1eea8e96f42b914723167cbad547ab1e
Charles Maina Mwangi <38659056+MainaMwangiy@users.noreply.github.com>
2023-01-02 22:31:47 +0300

    add edit & delete button actions on grid

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
-
-import { Box, Toolbar, Grid } from "@mui/material";
+import { Box, Toolbar, Grid, Button } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { DataGrid } from "@mui/x-data-grid";
 import axios from "axios";
 
+const onBtnClick = (e) => {
+  e.stopPropagation();
+};
+
+const onEdit = (e) => {
+  e.stopPropagation();
+};
 
 const columns = [
   { field: "id", headerName: "Id", width: 90 },
@@ -11,6 +17,28 @@ const columns = [
   { field: "email", headerName: "Email", width: 150, editable: true },
   { field: "contact", headerName: "contact", width: 150, editable: true },
   { field: "address", headerName: "address", width: 150, editable: true },
+  {
+    field: "actions",
+    headerName: "Actions",
+    width: 200,
+    renderCell: (params) => {
+      return (
+        <>
+          <Button onClick={(e) => onEdit(e, params.row)} variant="contained">
+            Edit
+          </Button>
+          <Button
+            color={"error"}
+            onClick={(e) => onBtnClick(e, params.row)}
+            variant="contained"
+            className="ml-2"
+          >
+            Delete
+          </Button>
+        </>
+      );
+    },
+  },
 ];
 
 const Home = () => {
@@ -18,11 +46,10 @@ const Home = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios("http://localhost:4000/work/get-employee")  
-      console.log(result)
-      setData(result.data.employee)
-    }
-    fetchData()
+      const result = await axios("http://localhost:4000/work/get-employee");
+      setData(result.data.employee);
+    };
+    fetchData();
   }, []);
   const grid = () => {
     return (
@@ -40,8 +67,8 @@ const Home = () => {
   return (
     <div>
       <Toolbar />
-      <Grid container spacing={2} sx={{ mt: 10, width: "50%", ml: "25%" }}>
-        <Box sx={{ height: 400, width: "100%" }}>{grid()}</Box>
+      <Grid container spacing={2} sx={{ mt: 10, width: "90%", ml: "5%" }}>
+        <Box sx={{ height: 450, width: "100%" }}>{grid()}</Box>
       </Grid>
     </div>
   );
